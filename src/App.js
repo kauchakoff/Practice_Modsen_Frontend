cb6c745989f95dbb6361a097d892d7ed6adcba4f
FedorC <fedor.sap@gmail.com>
2024-06-19 18:53:17 +0300

    Project structure changes

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="App">
+      <h2>React Application successfully launched.</h2>
+    </div>
+  );
+}
+
+export default App;
