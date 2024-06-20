02d4e6d7dbc05a8213b2322bbcb4ce78437df9af
kauchakoff <kauchakovie@gmail.com>
2024-06-20 17:18:01 +0300

    remade the router

--- src/utils/providers/RouterProvider/AppRouterProvider.jsx
+++ src/utils/providers/RouterProvider/AppRouterProvider.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider, Navigate } from 'react-router-dom';
 import Home from '../../../components/Home/Home';
 import Login from '../../../components/Login/Login';
 import Register from '../../../components/Register/Register';
@@ -15,17 +15,22 @@ const AppRouterProvider = () => {
     setUser(user);
   }, []);
 
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element = {user ? <Home setUser={setUser} /> : <Navigate to="/login"/>} />
-        <Route path="/login" element = {user ? <Navigate to="/" /> : <Login setUser={setUser} />}/>
-        <Route path="/register" element = {user ? <Navigate to="/" /> : <Register setUser={setUser} />}/>
-      </Routes>
-    </Router>
-  );
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: user ? <Home setUser={setUser} /> : <Navigate to="/login" />,
+    },
+    {
+      path: "/login",
+      element: user ? <Navigate to="/" /> : <Login setUser={setUser} />,
+    },
+    {
+      path: "/register",
+      element: user ? <Navigate to="/" /> : <Register setUser={setUser} />,
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 };
-
-
 
 export default AppRouterProvider;
